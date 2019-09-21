1740588070ec1b92b7c4aefd5d13d3175fad8fe1
Lara Yener <45222107+larayener@users.noreply.github.com>
2019-09-20 21:34:24 -0400

    Create counter.java

---
+++ counter.java
@@ -0,0 +1,16 @@
+
+public class Counter {
+	private int value;
+	
+	public Counter() {
+		value = 0;
+	}
+	
+	public void click() {
+		value++;
+	}
+	
+	public int getvalue() {
+		return value;
+	}
+}
